ecd27b26ae31d1331bab7eb307a25a6b1eee1e4d
Temesgen <temesgen.dessalegn@gmail.com>
2023-03-11 13:21:30 -0600

    Initial Commit

---
+++ src/main/java/com/olmaitsolutions/micro/springfullstack/SpringFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.olmaitsolutions.micro.springfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olmaitsolutions/micro/springfullstack/SpringFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olmaitsolutions.micro.springfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
